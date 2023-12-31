e5e576b65a37cda5a63aba4c5f4944fe5823f494
Sam Kopp <github@snxwman.dev>
2023-12-30 21:13:45 -0500

    more initial nvim lsp stuff

--- apps/nvim/lua/plugins/lsp.lua
+++ apps/nvim/lua/plugins/lsp.lua
@@ -59,20 +59,53 @@ return {
     },
     {
         "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "onsails/lspkind.nvim",
+        },
         config = function()
-            local cmp = require('cmp')
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            local lspkind = require("lspkind")
+
+            require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
+                completion = {
+                    completeopt = "menu, menuone, preview, noselect"
+                },
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end
+                },
+                formatting = {
+                    format = lspkind.cmp_format({
+                        maxwidth = 50,
+                        ellipsis_char = "…",
+                    }),
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
                 mapping = cmp.mapping.preset.insert({
                     -- `Enter` key to confirm completion
-                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+                    ["<CR>"] = cmp.mapping.confirm({select = false}),
 
                     -- Ctrl+Space to trigger completion menu
-                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ["<C-Space>"] = cmp.mapping.complete(),
 
                     -- Scroll up and down in the completion documentation
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
                 })
             })
         end
