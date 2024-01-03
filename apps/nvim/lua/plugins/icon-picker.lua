82cb078dc37c7e29a337472bd8887e7167014cb0
Sam Kopp <github@snxwman.dev>
2024-01-03 17:48:23 -0500

    update nvim plugin icon-picker config to include my fork for testing

--- apps/nvim/lua/plugins/icon-picker.lua
+++ apps/nvim/lua/plugins/icon-picker.lua
@@ -1,12 +1,30 @@
 return {
-    "ziontee113/icon-picker.nvim",
-    config = function()
-        require("icon-picker").setup({ disable_legacy_commands = true })
+    {
+        "ziontee113/icon-picker.nvim",
+        config = function()
+            require("icon-picker").setup({ disable_legacy_commands = true })
 
-        local opts = { noremap = true, silent = true }
+          local opts = { noremap = true, silent = true }
 
-        vim.keymap.set("n", "<Leader>i", "<cmd>IconPickerNormal nerd_font_v3<cr>", opts)
-        vim.keymap.set("n", "<Leader>y", "<cmd>IconPickerYank nerd_font_v3<cr>", opts)
-        vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
-    end
+            vim.keymap.set("n", "<Leader>i", "<cmd>IconPickerNormal nerd_font_v3<cr>", opts)
+            vim.keymap.set("n", "<Leader>y", "<cmd>IconPickerYank nerd_font_v3<cr>", opts)
+            vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
+        end
+    },
+--[[
+    {
+        "snxwman/icon-picker.nvim",
+        branch = "gen-nf-v3-icon-list",
+        enabled = true,
+        config = function()
+            require("icon-picker").setup({ disable_legacy_commands = true })
+
+            local opts = { noremap = true, silent = true }
+
+            vim.keymap.set("n", "<Leader>i", "<cmd>IconPickerNormal nerd_font_v3<cr>", opts)
+            vim.keymap.set("n", "<Leader>y", "<cmd>IconPickerYank nerd_font_v3<cr>", opts)
+            vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
+        end
+    }
+--]]
 }
