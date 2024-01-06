dcee622078706340ff68b0219055f7507e467633
Sam Kopp <github@snxwman.dev>
2024-01-05 19:51:00 -0500

    nvim: update icon-picker.lua config

--- apps/nvim/lua/plugins/icon-picker.lua
+++ apps/nvim/lua/plugins/icon-picker.lua
@@ -4,7 +4,7 @@ return {
         config = function()
             require("icon-picker").setup({ disable_legacy_commands = true })
 
-          local opts = { noremap = true, silent = true }
+            local opts = { noremap = true, silent = true }
 
             vim.keymap.set("n", "<Leader>i", "<cmd>IconPickerNormal nerd_font_v3<cr>", opts)
             vim.keymap.set("n", "<Leader>y", "<cmd>IconPickerYank nerd_font_v3<cr>", opts)
@@ -13,8 +13,9 @@ return {
     },
 --[[
     {
-        "snxwman/icon-picker.nvim",
-        branch = "gen-nf-v3-icon-list",
+        --"snxwman/icon-picker.nvim",
+        --branch = "gen-nf-v3-icon-list",
+        dir = "~/src/github/icon-picker.nvim/",
         enabled = true,
         config = function()
             require("icon-picker").setup({ disable_legacy_commands = true })
