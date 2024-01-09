e3422adca10192ab4550dcf2e64fd718887f9cc2
Sam Kopp <github@snxwman.dev>
2024-01-09 18:17:01 -0500

    nvim: move dressing.lua into ui.lua, add nvim-notify

---
+++ apps/nvim/lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "stevearc/dressing.nvim",
+        opts = {},
+    },
+    {
+        "rcarriga/nvim-notify",
+        opts = {},
+        config = function()
+            vim.opt.termguicolors = true
+        end
+    },
+}
