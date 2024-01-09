7443f7c4563ea64223b66c286493729154ecc878
Sam Kopp <github@snxwman.dev>
2024-01-09 18:18:36 -0500

    nvim: move tokyonight.nvim to theme.lua, add themery and other themes

--- apps/nvim/init.lua
+++ apps/nvim/init.lua
@@ -39,3 +39,4 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+vim.cmd("colorscheme tokyonight")
--- apps/nvim/lua/plugins/theme.lua
+++ apps/nvim/lua/plugins/theme.lua
@@ -1,12 +1,33 @@
-return { 
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("tokyonight").setup({
-            style = "night"
-        })
-
-        vim.cmd.colorscheme "tokyonight"
-    end
+return {
+    {
+        "zaldih/themery.nvim",
+        lazy = false,
+        opts = {
+            themes = {
+                "tokyonight",
+                "flexoki-dark",
+                "oxocarbon",
+                "moonfly"
+            },
+            themeConfigFile = "~/.config/nvim/lua/plugins/theme.lua",
+            livePreview = "true"
+        },
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = { style = "night" },
+    },
+    {
+        "kepano/flexoki-neovim",
+        name = "flexoki",
+    },
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+    },
+    { 
+        "bluz71/vim-moonfly-colors", 
+        name = "moonfly", 
+    },
 }
