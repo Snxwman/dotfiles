39b02cd87ed3b0d1eb9af0bc86725b8ea1f95629
Sam Kopp <github@snxwman.dev>
2024-01-09 18:24:01 -0500

    nvim: update icon-picker.lua config

--- apps/nvim/lua/plugins/icon-picker.lua
+++ apps/nvim/lua/plugins/icon-picker.lua
@@ -8,7 +8,7 @@ return {
 
             vim.keymap.set("n", "<Leader>i", "<cmd>IconPickerNormal nerd_font_v3<cr>", opts)
             vim.keymap.set("n", "<Leader>y", "<cmd>IconPickerYank nerd_font_v3<cr>", opts)
-            vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
+            vim.keymap.set("i", "<M-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
         end
     },
 --[[
@@ -24,7 +24,7 @@ return {
 
             vim.keymap.set("n", "<Leader>i", "<cmd>IconPickerNormal nerd_font_v3<cr>", opts)
             vim.keymap.set("n", "<Leader>y", "<cmd>IconPickerYank nerd_font_v3<cr>", opts)
-            vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
+            vim.keymap.set("i", "<M-i>", "<cmd>IconPickerInsert nerd_font_v3<cr>", opts)
         end
     }
 --]]
