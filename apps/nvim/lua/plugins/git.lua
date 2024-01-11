fcf679a9aab55f18c477e671395ff97d753b6609
Sam Kopp <github@snxwman.dev>
2024-01-10 19:43:14 -0500

    nvim(git.lua): add gitsigns.nvim and lazygit.nvim

---
+++ apps/nvim/lua/plugins/git.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {},
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+}
