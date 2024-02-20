ef75b75977a18876cfcf971b2ad74fe923e3b0da
Sam Kopp <github@snxwman.dev>
2024-02-19 20:14:27 -0500

    nvim: add hardtime plugin config

---
+++ apps/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,12 @@
+return {
+    "m4xshen/hardtime.nvim",
+    enabled = false,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim"
+    },
+    opts = {
+        max_count = 3,
+        disable_mouse = false,
+    },
+}
