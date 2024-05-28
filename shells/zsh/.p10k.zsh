8f557b5f914d3aef233e9811f57f7981ed5bca61
Sam Kopp <github@snxwman.dev>
2024-05-28 16:22:12 -0400

    p10k.zsh: update config

--- shells/zsh/.p10k.zsh
+++ shells/zsh/.p10k.zsh
@@ -31,8 +31,8 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    context                 # user@hostname
     os_icon                 # os identifier
+    context                 # user@hostname
     dir                     # current directory
     vcs                     # git status
     prompt_char           # prompt symbol
@@ -43,13 +43,13 @@
   # automatically hidden when the input line reaches it. Right prompt above the
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
-    status                  # exit code of the last command
-    command_execution_time  # duration of the last command
-    background_jobs         # presence of background jobs
-    direnv                  # direnv status (https://direnv.net/)
-    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-    anaconda                # conda environment (https://conda.io/)
+  #   status                  # exit code of the last command
+  #   command_execution_time  # duration of the last command
+  #   background_jobs         # presence of background jobs
+  #   direnv                  # direnv status (https://direnv.net/)
+  #   asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
+  #   virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+  #   anaconda                # conda environment (https://conda.io/)
     # pyenv                   # python environment (https://github.com/pyenv/pyenv)
     # goenv                   # go environment (https://github.com/syndbg/goenv)
     # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
@@ -92,8 +92,8 @@
     # midnight_commander      # midnight commander shell (https://midnight-commander.org/)
     # nix_shell               # nix shell (https://nixos.org/nixos/nix-pills/developing-with-nix-shell.html)
     # chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
-    vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
-    vpn_ip                  # virtual private network indicator
+    # vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
+    # vpn_ip                  # virtual private network indicator
     # load                  # CPU load
     # disk_usage            # disk usage
     # ram                   # free RAM
