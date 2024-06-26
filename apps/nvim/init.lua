f44bc1bd0538ff14784977633da31561e7edd1db
Sam Kopp <github@snxwman.dev>
2024-06-26 00:01:22 -0400

    nvim: set more options

--- apps/nvim/init.lua
+++ apps/nvim/init.lua
@@ -1,5 +1,11 @@
 local opt = vim.opt
 
+vim.g.have_nerd_font = true
+
+-- Keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- indentation
 opt.expandtab = true
 opt.shiftround = true
@@ -20,11 +26,19 @@ opt.relativenumber = true
 
 opt.cursorline = true
 
--- Keys
-vim.g.mapleader = " "
+opt.breakindent = true
 
--- Clipboard
+opt.list = true
+opt.listchars = { trail = "", nbsp = "␣", }
+
+vim.opt.hlsearch = true
+
+-- State
 opt.clipboard = "unnamedplus"
+opt.undofile = true
+
+opt.ignorecase = true
+opt.smartcase = true
 
 -- lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -47,7 +61,14 @@ vim.cmd("colorscheme tokyonight")
 vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
 vim.fn.sign_define("DiagnosticSignWarn",  {text = " ", texthl = "DiagnosticSignWarn"})
 vim.fn.sign_define("DiagnosticSignInfo",  {text = " ", texthl = "DiagnosticSignInfo"})
-vim.fn.sign_define("DiagnosticSignHint",  {text = "󰌵", texthl = "DiagnosticSignHint"})
+vim.fn.sign_define("DiagnosticSignHint",  {text = "󰌵 ", texthl = "DiagnosticSignHint"})
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 --[[
 vim.api.nvim_create_autocmd("VimEnter", {
