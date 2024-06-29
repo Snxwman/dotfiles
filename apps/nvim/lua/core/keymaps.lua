d80651ba590635dc9c9950768869c6daaf5db43e
Sam Kopp <github@snxwman.dev>
2024-06-28 20:57:02 -0400

    nvim: more keymaps

--- apps/nvim/lua/core/keymaps.lua
+++ apps/nvim/lua/core/keymaps.lua
@@ -1,9 +1,13 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("n", "<C-j>", "<C-d>zz")
 vim.keymap.set("n", "<C-k>", "<C-u>zz")
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<M-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<M-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<M-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<M-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "vaf", "gg0vG$", { desc = "Select the entire file" })
+vim.keymap.set("n", "yaf", "gg0vG$zy", { desc = "Copy the entire file" })
+vim.keymap.set("n", "daf", "gg0vG$d", { desc = "Delete the entire file" })
