22de6e6dec11d1579bb8e904412a04a03ae78dad
Sam Kopp <github@snxwman.dev>
2024-07-30 23:04:39 -0400

    nvim: add json plugin

---
+++ apps/nvim/lua/plugins/langs/json.lua
@@ -0,0 +1,7 @@
+return {
+    "Myzel394/jsonfly.nvim",
+    keys = {
+        {"<leader>j", "<cmd>Telescope jsonfly<cr>", mode = "n", ft = { "json", "xml", "yaml" }, desc = "Open json(fly)"},
+    },
+    opt = {},
+}
