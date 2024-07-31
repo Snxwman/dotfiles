10f87ddb7149b842117ce5790012f83b5d7770da
Sam Kopp <github@snxwman.dev>
2024-07-30 23:05:58 -0400

    nvim: minor plugin tweaks

--- apps/nvim/lua/plugins/whichkey.lua
+++ apps/nvim/lua/plugins/whichkey.lua
@@ -1,13 +1,18 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+    "folke/which-key.nvim",
+    dependencies = {
+        "echasnovski/mini.icons"
+    },
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        icons = {
+            rules = {
+                {pattern = "rust", icon = "", color = "orange"},
+            },
+        },
+    },
 }
