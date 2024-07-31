c5a49f0a71475cc89c56ab180bad65f0182ca3cb
Sam Kopp <github@snxwman.dev>
2024-07-30 23:02:56 -0400

    nvim: add obsidian.nvim

--- apps/nvim/lua/plugins/apps/obsidian.lua
+++ apps/nvim/lua/plugins/apps/obsidian.lua
@@ -1,1 +1,23 @@
-return {}
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",  -- recommended, use latest release instead of latest commit
+    lazy = true,
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      "BufReadPre " .. vim.fn.expand "~" .. "/documents/obsidian/**/**.md",
+      "BufNewFile " .. vim.fn.expand "~" .. "/documents/obsidian/**/**.md",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "Personal",
+                path = "~/documents/obsidian/Personal",
+            },
+        },
+        -- see below for full list of options 👇
+    },
+}
