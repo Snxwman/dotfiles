69726118eec27e123633a016bc091706d7956913
Sam Kopp <github@snxwman.dev>
2024-12-10 18:44:21 -0500

    nvim: fix colorizer showing color names in non-style files

--- apps/nvim/lua/plugins/colorizer.lua
+++ apps/nvim/lua/plugins/colorizer.lua
@@ -2,9 +2,20 @@ return {
     "NvChad/nvim-colorizer.lua",
     opts = {
         user_default_options = {
+            names = false,
             css = true,
-            sass = { enable = false, parsers = { "css" }, },
+            sass = {
+                enable = true,
+                parsers = { "css" },
+            },
+            tailwind = 'both',
             always_update = true,
-        }
-    }
+        },
+        filetypes = {
+            "*",
+            css = { names = true },
+            html = { names = true },
+            scss = { names = true },
+        },
+    },
 }
